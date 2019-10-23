91a442980d19756d3392466605e429f4769e2855
Rabbit <dawoodahmed696@gmail.com>
2019-10-23 21:56:29 +0500

    commented cutes code and add navbar here in APP VUE

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,106 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <!-- <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> -->
-    </div>
+    <!-- <div class="container-fluid"> -->
+      <div id="nav">
+          <Navbar  style="height:4rem;"/>
+
+        </div>
+
+    <!-- </div> -->
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+        <!-- <Navbar /> -->
     <router-view/>
+
   </div>
 </template>
 
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Navbar from '@/components/Navbar.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    Navbar
+  },
+  
+}
+</script>
+
 <style>
-@import url('https://fonts.googleapis.com/css?family=Nunito');
 #app {
-    font-family: "Nunito",sans-serif !important;
-    -webkit-font-smoothing: antialiased;
-    overflow-x: hidden;
-    position: relative;
-    height: 100%;
-    background: #F8F8F8;
-    scroll-behavior: smooth;
-}
-.text-primary {
-  color: #7AC8ED;
-}
-:root {
-  --danger: #7AC8ED;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 
-@media (min-width: 1200px) {
-    .container{
-        max-width: 1300px !important;
-    }
+html {
+  position: relative;
+  top: 0;
+  left: 0;
+  max-width: 100vw;
+  min-height: 200vh;
+  margin: 0px;
+  padding: 0px;
+  border: 0px;
+}
+body {
+  padding: 1.7vmin;
+  font-size: 16px;
+  font-family: sans, sans-serif;
+  color: #333;
+}
+
+a {
+  color: inherit;
+  font-style: italic;
+}
+
+nav {
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 3em;
+  width: 100%;
+  background-color: #EEE;
+  box-sizing: border-box;
+  padding: 12px 18px;
+  box-shadow: 0px 4px 7px #777;
+  transition: background-color 0.4s ease-out;
+}
+
+nav span {
+  font-weight: 600;
+  letter-spacing: .085em;
+}
+
+nav button {
+  position: absolute;
+  top: 0;
+  transform: translateY(50%);
+  right: 0;
+  min-height: 1.5em;
+  min-width: 80px;
+  background-color: #eee;
+  color: #222;
+  border: 0px;
+  border-radius: 2px;
+  margin-right: 18px;
+  text-transform: uppercase;
+}
+
+nav.scroll {
+  background-color: #A1572F;
+  color: #eef;
+}
+
+body > header {
+  margin-top: 4.7em;
 }
 
 </style>
